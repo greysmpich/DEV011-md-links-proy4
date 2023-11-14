e7d0e6634f3c5df307d4972ffcdd256ff54a1fd1
greysmpich <greys.mpich@gmail.com>
2023-11-14 17:10:03 -0600

    Se comprueba que la ruta existe

--- CLI.js
+++ CLI.js
@@ -1,6 +1,9 @@
 const { mdLinks } = require('./index.js')
+const fs = require('fs');
 const functions = require('./functions.js');
 
 const absolutePath = functions.absolutePath('./README.md');
-console.log('Ruta absoluta:', absolutePath);
+const pathExists = fs.existsSync(absolutePath)
+//console.log('Ruta absoluta:', absolutePath);
 
+mdLinks(pathExists).then((result) => {console.log(result);}).catch((error) => {console.log(error)});
