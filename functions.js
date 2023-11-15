65ae8d13e7ec410fea6bf5c3a2a6c216cae16e80
greysmpich <greys.mpich@gmail.com>
2023-11-15 13:46:55 -0600

    Quedó la validación isMarkdownFile

--- functions.js
+++ functions.js
@@ -8,3 +8,9 @@ exports.toAbsolutePath = (relativePath) => {
 exports.verifyPathExists = (absolutePath) => {
  return fs.existsSync(absolutePath)
 }
+
+exports.isMarkdownFile = (absolutePath) => {
+  const fileExtension = path.extname(absolutePath);
+  const markdownExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+  return markdownExtensions.includes(fileExtension);
+}
