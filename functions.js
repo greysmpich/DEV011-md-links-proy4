ffbe5a1960d479928c21680881f41b132f1fb612
greysmpich <greys.mpich@gmail.com>
2023-11-16 11:36:53 -0600

    Se lee el archivo markdown y se returna su contenido en consola

--- functions.js
+++ functions.js
@@ -1,16 +1,21 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
+const { readFile } = require("fs/promises");
 
 exports.toAbsolutePath = (relativePath) => {
-  return path.resolve(relativePath)
-}
+  return path.resolve(relativePath);
+};
 
 exports.verifyPathExists = (absolutePath) => {
- return fs.existsSync(absolutePath)
-}
+  return fs.existsSync(absolutePath);
+};
 
 exports.isMarkdownFile = (absolutePath) => {
   const fileExtension = path.extname(absolutePath);
-  const markdownExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+  const markdownExtensions = [".md", ".mkd", ".mdwn", ".mdown", ".mdtxt", ".mdtext", ".markdown", ".text"];
   return markdownExtensions.includes(fileExtension);
-}
+};
+
+exports.readMarkdownFile = (absolutePath) => {
+    return readFile(absolutePath, { encoding: "utf8" }); 
+};
